754e5d02c64611a3eda980823b5781e93a342c1b
Wix Remote Machine <support@wix.com>
2026-03-11 00:14:19 +0000

    Ajustar Background da Página

--- src/components/pages/LuxuryShowroomPage.tsx
+++ src/components/pages/LuxuryShowroomPage.tsx
@@ -12,16 +12,22 @@ export default function LuxuryShowroomPage() {
       <section 
         className="relative flex-1 w-full flex items-center justify-center overflow-hidden"
         style={{
-          backgroundImage: 'url(https://static.wixstatic.com/media/50f4bf_def07b5c61e349e690e31dcd9acd4861~mv2.png)',
-          backgroundSize: 'contain',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
           backgroundColor: '#0f141e',
           minHeight: 'calc(100vh - 80px)'
         }}
       >
+        {/* Background Image positioned to the right */}
+        <div
+          className="absolute inset-0 right-0 w-full md:w-2/3 lg:w-3/5"
+          style={{
+            backgroundImage: 'url(https://static.wixstatic.com/media/50f4bf_def07b5c61e349e690e31dcd9acd4861~mv2.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'right center',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
 
-        {/* Content centered on image */}
+        {/* Content centered on page */}
         <motion.div
           initial={{ opacity: 0, scale: 0.95 }}
           animate={{ opacity: 1, scale: 1 }}
