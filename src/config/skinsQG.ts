5be4ef24d13fd7fe19fb82f8800f5721ae1f91e1
Wix Remote Machine <support@wix.com>
2026-03-15 08:33:03 +0000

    Add SKINS_QG constant

---
+++ src/config/skinsQG.ts
@@ -0,0 +1,13 @@
+export const SKINS_QG = {
+  1:  "https://static.wixstatic.com/media/50f4bf_1776337cd2dc4ff1982d01b0079a48d2~mv2.png",
+  10: "https://static.wixstatic.com/media/50f4bf_6527240d26e94ca782357743f0ddddd7~mv2.png",
+  20: "https://static.wixstatic.com/media/50f4bf_b23aee963b00465fa534f7705505b5b9~mv2.png",
+  30: "https://static.wixstatic.com/media/50f4bf_b538b42955634d7190d28507d4b05023~mv2.png",
+  40: "https://static.wixstatic.com/media/50f4bf_86c3183c0550490fab41c5a8a8f6184b~mv2.png",
+  50: "https://static.wixstatic.com/media/50f4bf_f363ec9d5ca846c4990f7730c5bf479c~mv2.png",
+  60: "https://static.wixstatic.com/media/50f4bf_f36ccf79521242ab8518cf871e9f6a16~mv2.png",
+  70: "https://static.wixstatic.com/media/50f4bf_a8496fa280b84dbcac088278af9faded~mv2.png",
+  80: "https://static.wixstatic.com/media/50f4bf_8a605af9fc4646fd84c89e99c6acc4eb~mv2.png",
+  90: "https://static.wixstatic.com/media/50f4bf_dacc94520dfa449384a529f15de074f6~mv2.png",
+  100: "https://static.wixstatic.com/media/50f4bf_9683cd5787de47bf883c2453384fd2ae~mv2.png"
+} as const;
