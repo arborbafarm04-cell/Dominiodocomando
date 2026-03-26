2784f90208bf6d02f74c268ae053bcb3e440a968
arborbafarm-cmyk <arborbafarm@gmail.com>
2026-03-26 19:18:53 -0300

    Atualizar o mapStateStore.ts

--- src/store/mapStateStore.ts
+++ src/store/mapStateStore.ts
@@ -1,35 +1,44 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
 export interface MapState {
-  zoom: number;
-  panX: number;
-  panY: number;
-  setZoom: (zoom: number) => void;
-  setPan: (x: number, y: number) => void;
+  currentLocation: string;
+  mapZoom: number;
+  mapPosition: { x: number; y: number };
+
+  setCurrentLocation: (location: string) => void;
+  setMapZoom: (zoom: number) => void;
+  setMapPosition: (position: { x: number; y: number }) => void;
   resetToDefault: () => void;
 }
 
+const DEFAULT_LOCATION = 'home';
 const DEFAULT_ZOOM = 1;
-const DEFAULT_PAN_X = 0;
-const DEFAULT_PAN_Y = 0;
+const DEFAULT_POSITION = { x: 0, y: 0 };
 
-export const useMapStateStore = create<MapState>()(
-  persist(
-    (set) => ({
-      zoom: DEFAULT_ZOOM,
-      panX: DEFAULT_PAN_X,
-      panY: DEFAULT_PAN_Y,
-      setZoom: (zoom) => set({ zoom }),
-      setPan: (x, y) => set({ panX: x, panY: y }),
-      resetToDefault: () => set({ 
-        zoom: DEFAULT_ZOOM, 
-        panX: DEFAULT_PAN_X, 
-        panY: DEFAULT_PAN_Y 
-      }),
+export const useMapStateStore = create<MapState>((set) => ({
+  currentLocation: DEFAULT_LOCATION,
+  mapZoom: DEFAULT_ZOOM,
+  mapPosition: DEFAULT_POSITION,
+
+  setCurrentLocation: (location) =>
+    set({
+      currentLocation: location,
     }),
-    {
-      name: 'map-state-store',
-    }
-  )
-);
+
+  setMapZoom: (zoom) =>
+    set({
+      mapZoom: zoom,
+    }),
+
+  setMapPosition: (position) =>
+    set({
+      mapPosition: position,
+    }),
+
+  resetToDefault: () =>
+    set({
+      currentLocation: DEFAULT_LOCATION,
+      mapZoom: DEFAULT_ZOOM,
+      mapPosition: DEFAULT_POSITION,
+    }),
+}));
